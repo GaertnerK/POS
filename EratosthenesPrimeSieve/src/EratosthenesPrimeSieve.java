c2fc750b8a853432fe6e3c5c4294af992070e74e
GaertnerK <gaertnerk22039@sus.htl-grieskirchen.at>
2024-09-17 15:43:56 +0200

    Project finished

--- EratosthenesPrimeSieve/src/EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve/src/EratosthenesPrimeSieve.java
@@ -1,17 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class EratosthenesPrimeSieve implements PrimeSieve{
     private int border;
+    private boolean[] numbers;
 
     public EratosthenesPrimeSieve(int border) {
-        this.border = border;
+        this.border = border-2;
     }
 
     @Override
     public boolean isPrime(int p) {
+        numbers = new boolean[border];
+        for (int i = 0; i < numbers.length; i++){
+            numbers[i] = true;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            int number = i + 2;
+            while (number <= border){
+                number += (i + 2);
+                if (number > border){
+                    break;
+                }
+                numbers[number-2] = false;
+            }
+        }
+        for (int i = 2; i < numbers.length; i++){
+            if (i == p){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public void printPrimes() {
+        int counter = 0;
+        System.out.println("Primes:");
+        for (int i = 2; i < numbers.length; i++){
+            if (numbers[i-2] == true){
+                System.out.print(i + " ");
+                counter++;
 
+            }
+            if (counter == 10){
+                System.out.println();
+                counter = 0;
+            }
+        }
     }
 }
--- EratosthenesPrimeSieve/src/Main.java
+++ EratosthenesPrimeSieve/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EratosthenesPrimeSieve eratosthenesPrimeSieve = new EratosthenesPrimeSieve(120);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int n = in.nextInt();
+        boolean isPrime = eratosthenesPrimeSieve.isPrime(n);
+        if (isPrime == true){
+            System.out.println("Prime" + "\n");
+        }else{
+            System.out.println("Not Prime" + "\n");
+        }
+        eratosthenesPrimeSieve.printPrimes();
     }
 }
