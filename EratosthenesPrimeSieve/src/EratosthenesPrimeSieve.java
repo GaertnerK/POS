25ada96cd342e0b62510ae826700b5e0661d9a5a
GaertnerK <gaertnerk22039@sus.htl-grieskirchen.at>
2024-09-17 14:36:41 +0200

    EratosthenesPrimeSieve added

---
+++ EratosthenesPrimeSieve/src/EratosthenesPrimeSieve.java
@@ -0,0 +1,17 @@
+public class EratosthenesPrimeSieve implements PrimeSieve{
+    private int border;
+
+    public EratosthenesPrimeSieve(int border) {
+        this.border = border;
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
+}
