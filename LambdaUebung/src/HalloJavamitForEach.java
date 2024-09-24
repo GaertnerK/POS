21acedd98c98674621457e25b0022db353fd7065
GaertnerK <gaertnerk22039@sus.htl-grieskirchen.at>
2024-09-24 14:18:28 +0200

    HalloJavamitForEach created

---
+++ LambdaUebung/src/HalloJavamitForEach.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavamitForEach {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("Hallo");
+        list.add("Java");
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println();
+        
+        list.forEach((String s) -> System.out.println(s));
+        System.out.println();
+
+        list.forEach(System.out::println);
+    }
+}
