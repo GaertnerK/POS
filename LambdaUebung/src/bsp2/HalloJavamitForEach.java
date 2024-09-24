c321e1eb59c081c8959baa962cba744a1a3a5097
GaertnerK <gaertnerk22039@sus.htl-grieskirchen.at>
2024-09-24 14:22:42 +0200

    Packages bsp2 and bsp3 created

--- LambdaUebung/src/bsp2/HalloJavamitForEach.java
+++ LambdaUebung/src/bsp2/HalloJavamitForEach.java
@@ -1,16 +1,18 @@
+package bsp2;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class HalloJavamitForEach {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<>();
         list.add("Hallo");
         list.add("Java");
         for (String s : list) {
             System.out.println(s);
         }
         System.out.println();
-        
+
         list.forEach((String s) -> System.out.println(s));
         System.out.println();
 
