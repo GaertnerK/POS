d00006962b55bc98bff3842086b505b75ce040fd
GaertnerK <gaertnerk22039@sus.htl-grieskirchen.at>
2024-11-05 14:00:40 +0100

    Project started

--- Concurrency/src/bsp1/Main.java
+++ Concurrency/src/bsp1/Main.java
@@ -1,7 +1,15 @@
 package bsp1;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+
     }
 }
